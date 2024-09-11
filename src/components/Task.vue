274656cfcdcfa4d76738175e7c92910ab9a13ef4
RuslanHamidov <rhamidov202@gmail.com>
2024-09-12 01:55:38 +0400

    initial commit

---
+++ src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+    <ul>
+      <li v-for="task in jsonData" :key="task.id">
+        {{ task.task }} - {{ task.date }}
+      </li>
+    </ul>
+ </div>
+</template>
+
+
+<script>
+import jsonData from '../../data.json';
+
+export default {
+    data() {
+        return {
+            jsonData
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
